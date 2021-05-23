eae74f82716eb58e4c00b8d8f15b022de5a2bce1
Lunotte <charlybeaugrand@gmail.com>
2021-05-24 01:45:49 +0200

    Configurer l'ajout du token dans les requetes http

--- src/config/AxiosConfig.ts
+++ src/config/AxiosConfig.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import params from '../config/parametre.json'
-import { getStorage } from '../services/StorageService';
 
 export const instance = axios.create({
     baseURL: params.baseUrl
@@ -22,18 +21,8 @@ instance.interceptors.request.use(
       return Promise.reject(error);
   });
   
-  async function getToken() {
-    const token = await getStorage('token');
-    
-    if (token == null) {
-      return null;
-    } else {
-      return token;
-    }
-  }
-  
   export async function header() {
-    const token = await getToken();
+    const token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzZWN1cmUtYXBpIiwiYXVkIjoic2VjdXJlLWFwcCIsInN1YiI6InVzZXJuYW1lMSIsImV4cCI6MTYyMjY1NTA2NSwicm9sIjpbXX0.7yBkKodS0VCSrgUCRkG0Y5cg4kOcV7MSFZpGEt6AuonTrpRYxc8OGPT2lfeLkkwSq6cEoNwTXZEzX2npQc6p0w';
     
     let headers = {};
     
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { utiliserConfigAction } from './redux/hook-action';
-export { useRequestWithConfigAxios, useToLoadConfig } from './effects';
+export { useRequestWithConfigAxios, useToLoadConfig, useRequestWithoutDispatch2 } from './effects';
 export { useToTriggerConfig } from './tools/main';
-export { default as hookReducer} from './redux/hook-reducer';
+export { default as hookReducer } from './redux/hook-reducer';
+export { default as StoreComponent } from './store.component';
