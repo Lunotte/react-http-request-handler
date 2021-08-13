b5e662671a340b2811e5daf87272789de96ba987
Lunotte <charlybeaugrand@gmail.com>
2021-08-13 22:38:48 +0200

    Correction compilation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 /*
  * File: index.ts                                                              *
  * Project: react-http-request-handler                                         *
- * Created Date: Su Jun yyyy                                                   *
- * Author: <<author>                                                           *
+ * Created Date: Sun June 13 2021                                              *
+ * Author:  Charly Beaugrand                                                   *
  * -----                                                                       *
- * Last Modified: Sun Aug 08 2021                                              *
+ * Last Modified: Fri Aug 13 2021                                              *
  * Modified By: Charly Beaugrand                                               *
  * -----                                                                       *
  * Copyright (c) 2021 Lunotte                                                  *
@@ -18,7 +18,5 @@ export { ConfigQueryParameter, MethodRnhrh, MultiplePossibleValues, ParamRnhnh, 
 export type { ActionToDispatch, ErreurFetchApi, KeyValue, ResponseFetchApi, Rh2AxiosConfig, Rh2InitializationParameter, Rh2State } from './models';
 export { default as InitializerRnhrh } from './Rh2Initializer';
 export { rh2AxiosConfigService, rh2ConfigService } from './services';
-export {
-    useRh2WithName, useRh2WithNameTakeParamsInRoute, useRh2WithParameters, useRh2WithParametersTakeParamsInRoute
-} from './services/Rh2EffectsService';
+export { useRh2WithName, useRh2WithParameters } from './services/Rh2EffectsService';
 
